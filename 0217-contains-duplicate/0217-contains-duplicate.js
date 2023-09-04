b3e7513f754353193ea1bf179b80edf36e3745f8
Satria <92832439+notsatria@users.noreply.github.com>
2023-09-04 10:09:15 +0700

    Time: 168 ms (22.96%), Space: 52.1 MB (77.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    nums = nums.sort();
+    
+    for (let i = 1; i <= nums.length; i++) {
+        
+        if (nums[i] === nums[i - 1]) {
+            return true;
+        }
+        
+    }
+    
+    return false;
+    
+    
+    
+};
