4f03de8b7ff5c7f9cc9617445a7ca4631b1b0b6a
Satria <92832439+notsatria@users.noreply.github.com>
2024-06-17 09:45:30 +0700

    Time: 518 ms (5.08%), Space: 53.1 MB (5.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,18 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map();
+    const pairs = new Map();
+    const result = [];
     
-    for (var i = 0; i < nums.length; i++) {
-        map.set(nums[i], i);
+    for (let i = 0; i < nums.length; i++) {
+        pairs.set(i, nums[i]);
+
+        // for (let value of pairs.values()) {
+        //     if (value === target - nums[i]) {
+        //         console.log(value);
+        //     }
+        // }
+        pairs.forEach((value, key) => {
+            if (value === target - nums[i] && key != i) {
+                console.log([key, i]);
+                result.push(...[key, i])
+            }
+        })
     }
     
-    for (var i = 0; i < nums.length; i++) {
-        var complement = target - nums[i];
-        
-        if (map.has(complement) && (map.get(complement) !== i)) {
-            return [i, map.get(complement)];
-        }
-    }
-    
+    return result;
+
 };
