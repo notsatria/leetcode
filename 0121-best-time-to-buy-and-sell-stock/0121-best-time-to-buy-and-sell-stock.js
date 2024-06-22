7bae45ef2c95446c0929f41091f9bcdd117a347e
Satria <92832439+notsatria@users.noreply.github.com>
2024-06-22 11:32:25 +0700

    Time: 76 ms (49.95%), Space: 58.7 MB (81.95%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = Infinity
+    let maxProfit = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        }
+        
+        let potentialProfit = prices[i] - minPrice
+        
+        if (potentialProfit > maxProfit) {
+            maxProfit = potentialProfit
+        }
+    }
+    
+    return maxProfit
+};
